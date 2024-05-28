d54a9df66ff89b2daff4e1177dd699ca5ddcdb69
choieuntaek <choiet0225@gmail.com>
2024-05-28 22:02:00 +0900

    feat: dev

--- Insurance_2024_1/src/domain/Contract.java
+++ Insurance_2024_1/src/domain/Contract.java
@@ -1,7 +1,9 @@
 package domain;
 public class Contract {
 	private int concludeEID;
-	private int contractDate;
+	private int createContractEID;
+	private String createdDate;
+	private String concludedDate;
 	private int contractID;
 	private String contractStatus;
 	private int customerID;
@@ -17,151 +19,67 @@ public class Contract {
 	private String resurrectionDate;
 	private String resurrectionReason;
 	private int underwritingEID;
-	// 
+	// association
 	public Payment m_Payment;
 	public PaymentInfo m_PaymentInfo;
-	public Contract(){
+	public Contract(){}
+	public void finalize() throws Throwable {}
+	public boolean endorsementReview(int 계약ID){return false;}
+	public void permitContract(int ContractID){}
 
-	}
-	public void finalize() throws Throwable {
-
-	}
-	public boolean endorsementReview(int 계약ID){
-		return false;
-	}
-
-	public void permitContract(int ContractID){
-
-	}
-
+	// operater
 	public void processUnderwriting(int contractID){
-
 	}
 
 	public void requestReUnderwriting(int contractID){
-
 	}
 
 	public void revive(Contract contract){
-
 	}
 
 	public void saveRenewalStatus(boolean renewalStatus){
-
 	}
 	//
-	public int getConcludeEID() {
-		return concludeEID;
-	}
-	public void setConcludeEID(int concludeEID) {
-		this.concludeEID = concludeEID;
-	}
-	public int getContractDate() {
-		return contractDate;
-	}
-	public void setContractDate(int contractDate) {
-		this.contractDate = contractDate;
-	}
-	public int getContractID() {
-		return contractID;
-	}
-	public void setContractID(int contractID) {
-		this.contractID = contractID;
-	}
-	public String getContractStatus() {
-		return contractStatus;
-	}
-	public void setContractStatus(String contractStatus) {
-		this.contractStatus = contractStatus;
-	}
-	public int getCustomerID() {
-		return customerID;
-	}
-	public void setCustomerID(int customerID) {
-		this.customerID = customerID;
-	}
-	public String getEvaluation() {
-		return evaluation;
-	}
-	public void setEvaluation(String evaluation) {
-		this.evaluation = evaluation;
-	}
-	public String getExpirationDate() {
-		return expirationDate;
-	}
-	public void setExpirationDate(String expirationDate) {
-		this.expirationDate = expirationDate;
-	}
-	public int getInsuranceID() {
-		return insuranceID;
-	}
-	public void setInsuranceID(int insuranceID) {
-		this.insuranceID = insuranceID;
-	}
-	public boolean isConclude() {
-		return isConclude;
-	}
-	public void setConclude(boolean isConclude) {
-		this.isConclude = isConclude;
-	}
-	public boolean isPassUW() {
-		return isPassUW;
-	}
-	public void setPassUW(boolean isPassUW) {
-		this.isPassUW = isPassUW;
-	}
-	public int getMonthlyPremium() {
-		return monthlyPremium;
-	}
-	public void setMonthlyPremium(int monthlyPremium) {
-		this.monthlyPremium = monthlyPremium;
-	}
-	public int getNonPaymentPeriod() {
-		return nonPaymentPeriod;
-	}
-	public void setNonPaymentPeriod(int nonPaymentPeriod) {
-		this.nonPaymentPeriod = nonPaymentPeriod;
-	}
-	public PaymentInfo getPaymentInfo() {
-		return paymentInfo;
-	}
-	public void setPaymentInfo(PaymentInfo paymentInfo) {
-		this.paymentInfo = paymentInfo;
-	}
-	public boolean isRenewalStatus() {
-		return renewalStatus;
-	}
-	public void setRenewalStatus(boolean renewalStatus) {
-		this.renewalStatus = renewalStatus;
-	}
-	public String getResurrectionDate() {
-		return resurrectionDate;
-	}
-	public void setResurrectionDate(String resurrectionDate) {
-		this.resurrectionDate = resurrectionDate;
-	}
-	public String getResurrectionReason() {
-		return resurrectionReason;
-	}
-	public void setResurrectionReason(String resurrectionReason) {
-		this.resurrectionReason = resurrectionReason;
-	}
-	public int getUnderwritingEID() {
-		return underwritingEID;
-	}
-	public void setUnderwritingEID(int underwritingEID) {
-		this.underwritingEID = underwritingEID;
-	}
-	public Payment getM_Payment() {
-		return m_Payment;
-	}
-	public void setM_Payment(Payment m_Payment) {
-		this.m_Payment = m_Payment;
-	}
-	public PaymentInfo getM_PaymentInfo() {
-		return m_PaymentInfo;
-	}
-	public void setM_PaymentInfo(PaymentInfo m_PaymentInfo) {
-		this.m_PaymentInfo = m_PaymentInfo;
-	}
+	public int getConcludeEID() {return concludeEID;}
+	public void setConcludeEID(int concludeEID) {this.concludeEID = concludeEID;}
+	public String getCreatedDate() {return createdDate;}
+	public void setCreatedDate(String createdDate) {this.createdDate = createdDate;}
+	public String getConcludedDate() {return concludedDate;}
+	public void setConcludedDate(String concludedDate) {this.concludedDate = concludedDate;}
+	public int getContractID() {return contractID;}
+	public void setContractID(int contractID) {this.contractID = contractID;}
+	public int getCreateContractEID() {return createContractEID;}
+	public void setCreateContractEID(int createContractEID) {this.createContractEID = createContractEID;}
+	public String getContractStatus() {return contractStatus;}
+	public void setContractStatus(String contractStatus) {this.contractStatus = contractStatus;}
+	public int getCustomerID() {return customerID;}
+	public void setCustomerID(int customerID) {this.customerID = customerID;}
+	public String getEvaluation() {return evaluation;}
+	public void setEvaluation(String evaluation) {this.evaluation = evaluation;}
+	public String getExpirationDate() {return expirationDate;}
+	public void setExpirationDate(String expirationDate) {this.expirationDate = expirationDate;}
+	public int getInsuranceID() {return insuranceID;}
+	public void setInsuranceID(int insuranceID) {this.insuranceID = insuranceID;}
+	public boolean isConclude() {return isConclude;}
+	public void setConclude(boolean isConclude) {this.isConclude = isConclude;}
+	public boolean isPassUW() {return isPassUW;}
+	public void setPassUW(boolean isPassUW) {this.isPassUW = isPassUW;}
+	public int getMonthlyPremium() {return monthlyPremium;}
+	public void setMonthlyPremium(int monthlyPremium) {this.monthlyPremium = monthlyPremium;}
+	public int getNonPaymentPeriod() {return nonPaymentPeriod;}
+	public void setNonPaymentPeriod(int nonPaymentPeriod) {this.nonPaymentPeriod = nonPaymentPeriod;}
+	public PaymentInfo getPaymentInfo() {return paymentInfo;}
+	public void setPaymentInfo(PaymentInfo paymentInfo) {this.paymentInfo = paymentInfo;}
+	public boolean isRenewalStatus() {return renewalStatus;}
+	public void setRenewalStatus(boolean renewalStatus) {this.renewalStatus = renewalStatus;}
+	public String getResurrectionDate() {return resurrectionDate;}
+	public void setResurrectionDate(String resurrectionDate) {this.resurrectionDate = resurrectionDate;}
+	public String getResurrectionReason() {return resurrectionReason;}
+	public void setResurrectionReason(String resurrectionReason) {this.resurrectionReason = resurrectionReason;}
+	public int getUnderwritingEID() {return underwritingEID;}
+	public void setUnderwritingEID(int unde$rwritingEID) {this.underwritingEID = underwritingEID;}
+	public Payment getM_Payment() {return m_Payment;}
+	public void setM_Payment(Payment m_Payment) {this.m_Payment = m_Payment;}
+	public PaymentInfo getM_PaymentInfo() {return m_PaymentInfo;}
+	public void setM_PaymentInfo(PaymentInfo m_PaymentInfo) {this.m_PaymentInfo = m_PaymentInfo;}
 }
--- Insurance_2024_1/src/domain/Customer.java
+++ Insurance_2024_1/src/domain/Customer.java
@@ -30,7 +30,7 @@ public class Customer {
 	public MedicalHistory m_MedicalHistory;
 	public PaymentList paymentListImpl;
 	public InsuranceList insuranceListImpl;
-  public AccidentList accidentListImpl;
+	public AccidentList accidentListImpl;
 	public CompensationList compensationListImpl;
 	
 	public Customer(){
@@ -134,27 +134,6 @@ public class Customer {
 	}
 	public void setM_Contract(Contract m_Contract) {
 		this.m_Contract = m_Contract;
-	}
-	public MedicalHistory getM_MedicalHistory() {
-		return m_MedicalHistory;
-	}
-	public void setM_MedicalHistory(MedicalHistory m_MedicalHistory) {
-		this.m_MedicalHistory = m_MedicalHistory;
-	}
-	public void setCounselList(CounselList counselListImpl) {
-		this.counselListImpl = counselListImpl;
-	}
-	public boolean requestCounsel(Counsel counsel) {
-		return counselListImpl.add(counsel);
-	}
-	public boolean deleteCounsel(int counselID) {
-		return counselListImpl.delete(counselID);
-	}
-	public void setPaymentList(PaymentList paymentListImpl) {
-		this.paymentListImpl = paymentListImpl;
-	}
-	public void setInsuranceList(InsuranceList insuranceListImpl) {
-		this.insuranceListImpl = insuranceListImpl;
 	}
 	public void setAccidentList(AccidentList accidentListImpl) {
 		this.accidentListImpl = accidentListImpl;
