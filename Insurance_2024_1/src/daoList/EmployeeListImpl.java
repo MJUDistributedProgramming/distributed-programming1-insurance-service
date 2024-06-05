8a4c7d68b9e5683702ab16550f881b7df2ec387b
JunnKyuu <junnkyuu22@gmail.com>
2024-06-05 18:48:41 +0900

    오류 수정

--- Insurance_2024_1/src/daoList/EmployeeListImpl.java
+++ Insurance_2024_1/src/daoList/EmployeeListImpl.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 import IF.EmployeeList;
 import domain.Customer;
 import domain.Employee;
+import exception.DuplicateIDException;
 public class EmployeeListImpl implements EmployeeList {
 	private ArrayList<Employee> EmployeeList;
 	public Employee m_Employee;
@@ -14,24 +15,27 @@ public class EmployeeListImpl implements EmployeeList {
 	public ArrayList<Employee> retrieveAll(){
 		return EmployeeList;
 	}
-	public boolean add(Employee Employee){
+	public String add(Employee Employee) throws DuplicateIDException{
 		for (Employee employee : EmployeeList) {
 			if (employee.getEmployeeID() == Employee.getEmployeeID()) {
-				return false;
+				throw new DuplicateIDException();
 			}
 		}
 		this.EmployeeList.add(Employee);
-		return true;
+		return "[success] 회원가입에 성공하였습니다.";
 	}
-	public boolean deleteById(int id){
+	public String deleteById(int id){
 		Employee deleteEmployee = null;
 		for (Employee employee : EmployeeList) {
 			if (employee.getEmployeeID() == id) {
 				deleteEmployee = employee;
 			}
 		}
+		if (deleteEmployee==null) {
+			return "[error] 해당 id의 직원정보가 존재하기 않습니다.";
+		}
 		EmployeeList.remove(deleteEmployee);
-		return true;
+		return "[success] 해당 직원정보가 삭제되었습니다.";
 	}
 	public Employee retrieveById(int id){
 		for (Employee employee : EmployeeList) {
